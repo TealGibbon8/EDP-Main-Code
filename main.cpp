30726620e7530905e85a063315cd788f9db09e2a
Alasdair Morrison <alasdair.morrison1002@gmail.com>
2025-02-07 15:59:32 +0000

    Working code without filtering or averaging

--- main.cpp
+++ main.cpp
@@ -2,11 +2,12 @@
 #include "TextLCD.h"
 
 // Blinking rate in milliseconds
-#define BLINKING_RATE     1ms
+#define WAIT_TIME    500us
 TextLCD lcd(p26, p25, p24, p23, p22, p21, TextLCD::LCD16x2);
 AnalogIn SigIn(p17);
 AnalogOut SigOut(p18);
 Ticker TimerInt;
+Ticker PkRstTimer;
 //TextLCD lcd(REGSEL, ENABLE, MSB1, MSB2, MSB3, MSB4), TextLCD::LCD16x2);
 //register select p26, LCD pin4
 // Enable p25, LCD pin 6
@@ -18,8 +19,8 @@ int c; //A variable to count with, do not use count, it is something else entire
 float signal = SigIn.read();
 float previous = 0;
 float current = 0;
-float alpha = 0.2;
-float inputs[10]; //number in brackets sets number of singals to include in average
+float alpha = 1.0;
+float inputs[5]; //number in brackets sets number of singals to include in average
 int ValsStored = 0;
 int numOfVals = 10;
 float averaged = 0;
@@ -46,11 +47,19 @@ void RollingAverage() {
         averaged = sum / numOfVals;
         ValsStored = 0;
     }
+    else{
+        averaged = 0;
+    }
 }
 
 void printer() {
     lcd.printf("input  %.3f V\n", signal*3.3);
-    lcd.printf("output %.3f V\n", averaged*3.3);
+    lcd.printf("output %.3f V\n", output*3.3);
+}
+
+void PKRst() {
+    minValue = 0;
+    maxValue = 0;
 }
 
 int main()
@@ -63,85 +72,62 @@ int main()
     // DigitalOut led3(LED3);
     // DigitalOut led4(LED4);
     // c = 0;
-    //TimerInt.attach(&printer, 5000us); // setup ticker to call flip at 50uS
+    //TimerInt.attach(&printer, 5ms); // setup ticker to call printer at 5mS
+    //PkRstTimer.attach(&PKRst, 2s);
 
     while (true) {
         signal = SigIn.read();
-        /*
-        led1 = !led1; // debug lights, change when changing code to show the code has updated.
-        led2 = !led2;
-        led3 = !led3;
-        led4 = !led4;
-        lcd.printf("Hello  %d \n", c); //prints hello and a number first line of display
-        ++c; //increases the number
-        lcd.printf("Hello  %d \n", c); // prints to the second line
-        ThisThread::sleep_for(BLINKING_RATE); //waits for a time, possible to interupt?
-        */
-        /*
-        maxi = 0;
-        mini = 1;
-        if (signal > maxi) {
-            maxi = signal;
-        }
-        if (signal < mini) {
-            mini = signal;
-        }
-        stepSize = (maxi-mini)/8;
-
+    
         FilterSignal();
-        RollingAverage();
-        if(averaged != 0) {
-            output = averaged * stepSize;
-        }
-        else {output = 0;}
-        */
-        FilterSignal();
-        RollingAverage();
+        //RollingAverage();
+        averaged = current;
 
-        if (averaged < minValue) {minValue = averaged;}
-        if (averaged > maxValue) {maxValue = averaged;}
-        int digiOut = 0;
+        if(averaged == 0) {}
+        else {
+            if (averaged < minValue) {minValue = averaged;}
+            if (averaged > maxValue) {maxValue = averaged;}
 
-        ThisThread::sleep_for(2ms);
+            int digiOut = 0;
+            float PkPk = maxValue - minValue;
 
-        float PkPk = maxValue - minValue;
-        if (averaged < minValue+(PkPk*1/8)){
-            output=minValue;
-            digiOut = 0;
+            if (averaged < minValue+(PkPk*1/8)){
+                output=minValue;
+                digiOut = 0;
+            }
+            else if (averaged > minValue+(PkPk*1/8) && averaged <= minValue+(PkPk*2/8)){
+                output=minValue+(PkPk*1/7);
+                digiOut = 1;
+            }
+            else if (averaged > minValue+(PkPk*2/8) && averaged <= minValue+(PkPk*3/8)){
+                output=minValue+(PkPk*2/7);
+                digiOut = 2;
+            }
+            else if (averaged > minValue+(PkPk*3/8) && averaged <= minValue+(PkPk*4/8)){
+                output=minValue+(PkPk*3/7);
+                digiOut = 3;
+            }
+            else if (averaged > minValue+(PkPk*4/8) && averaged <= minValue+(PkPk*5/8)){
+                output=minValue+(PkPk*4/7);
+                digiOut = 4;
+            }
+            else if (averaged > minValue+(PkPk*5/8) && averaged <= minValue+(PkPk*6/8)){
+                output=minValue+(PkPk*5/7);
+                digiOut = 5;
+            }
+            else if (averaged > minValue+(PkPk*6/8) && averaged <= minValue+(PkPk*7/8)){
+                output=minValue+(PkPk*6/7);
+                digiOut = 6;
+            }
+            else if (averaged > minValue+(PkPk*7/8)){
+                output=minValue+(PkPk);
+                digiOut = 7;
+            }
+            SigOut.write(output);
         }
-        else if (averaged > minValue+(PkPk*1/8) && averaged <= minValue+(PkPk*2/8)){
-            output=minValue+(PkPk*1/7);
-            digiOut = 1;
-        }
-        else if (averaged > minValue+(PkPk*2/8) && averaged <= minValue+(PkPk*3/8)){
-            output=minValue+(PkPk*2/7);
-            digiOut = 2;
-        }
-        else if (averaged > minValue+(PkPk*3/8) && averaged <= minValue+(PkPk*4/8)){
-            output=minValue+(PkPk*3/7);
-            digiOut = 3;
-        }
-        else if (averaged > minValue+(PkPk*4/8) && averaged <= minValue+(PkPk*5/8)){
-            output=minValue+(PkPk*4/7);
-            digiOut = 4;
-        }
-        else if (averaged > minValue+(PkPk*5/8) && averaged <= minValue+(PkPk*6/8)){
-            output=minValue+(PkPk*5/7);
-            digiOut = 5;
-        }
-        else if (averaged > minValue+(PkPk*6/8) && averaged <= minValue+(PkPk*7/8)){
-            output=minValue+(PkPk*6/7);
-            digiOut = 6;
-        }
-        else if (averaged > minValue+(PkPk*7/8)){
-            output=minValue+(PkPk);
-            digiOut = 7;
-        }
-        SigOut.write(output);
-        // lcd.printf("input  %.3f V\n", signal*3.3);
-        // lcd.printf("output %.3f V\n", output*3.3);
+        lcd.printf("min  %.3f V\n", minValue*3.3);
+        lcd.printf("max %.3f V\n", maxValue*3.3);
         //lcd.printf("Da Dum Da Dum\nGroup 4\n");
-        ThisThread::sleep_for(100ms);
+        //wait_us(500);
     }
 }
 
